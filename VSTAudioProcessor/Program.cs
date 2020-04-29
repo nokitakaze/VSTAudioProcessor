b57b5ae9b431d1c04ff8afe14aa0e19a2057bbdc
Nokita Kaze <admin@kanaria.ru>
2020-04-29 20:58:58 +0300

    * Init commit

---
+++ VSTAudioProcessor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Jacobi.Vst.Core;
+
+namespace VSTAudioProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
